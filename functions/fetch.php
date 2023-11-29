67d1cc4bcb76961165601df11f78a21b14da795b
davem4tth3w <davemmatthew0807@gmail.com>
2023-11-29 15:48:53 +0800

    Update fetch.php

--- functions/fetch.php
+++ functions/fetch.php
@@ -1,5 +1,7 @@
 <?php
     //fetching data
     $query = "SELECT * FROM tblsubject LEFT JOIN tblsyllabus ON tblsyllabus.subject_id = tblsubjects.subjectid";
-    $result = mysqli_query($dbc, $query); //using mysqli_query
+    $result = mysqliquery($dbc, $query); //using mysqli_query
 ?>
+
+<!-- $result = mysqli_query($dbc, $query); -->
