6e3528e7e7da67b7b0b8cb6e8e134a26b14b206f
Jude-Vills <qb202100192@wmsu.edu.ph>
2023-11-29 14:37:12 +0800

    updated

--- functions/fetch.php
+++ functions/fetch.php
@@ -1,5 +1,5 @@
 <?php
     //fetching data
-    $query = "SELECT * FROM tblsubject";
+    $query = "SELECT * FROM tblsubject LEFT JOIN tblsyllabus ON tblsyllabus.subject_id = tblsubjects.subjectid";
     $result = mysqli_query($dbc, $query); //using mysqli_query
 ?>
