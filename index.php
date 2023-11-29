ccaf280f4eeec6e4fe43420dc8811612d68984a8
Jude-Vills <qb202100192@wmsu.edu.ph>
2023-11-29 13:49:03 +0800

    updated index.php, added two php files

--- index.php
+++ index.php
@@ -12,11 +12,14 @@
     <body>
         <h1>My Subjects</h1>
         <a href="forms/addform.php">Add Subject</a><br/><br/>
-        <table width='100%' border= 1>
-            <tr bgcolor='#CCCCCC'>
+        <table width='100%'>
+            <tr>
                 <td>ID</td>
                 <td>Subject Code</td>
                 <td>Subject Name</td>
+                <td>Subject ID</td>
+                <td>Subject Code</td>
+                <td>Subject Author</td>
                 <td>Action</td>
             </tr>
             <?php
@@ -25,6 +28,9 @@
                     echo "<td>".$res['subject_id']."</td>";
                     echo "<td>".$res['subject_code']."</td>";
                     echo "<td>".$res['subject_name']."</td>";
+                    echo "<td>".$res['syllabus_id']."</td>";
+                    echo "<td>".$res['syllabus_code']."</td>";
+                    echo "<td>".$res['syllabus_author']."</td>";
                     echo "<td><a href=\forms/editform.php?id=$res[subject_id]\">Edit</a> |
                               <a href=\forms/editform.php?id=$res[subject_id]\"
                               onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>"; 
